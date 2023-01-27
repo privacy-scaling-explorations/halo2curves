b86a8c1953e301bc04a6341d4995482ce02a43f3
CPerezz <c.perezbaro@gmail.com>
2023-01-27 11:40:05 +0100

    chore: Remove tonelly-shanks local algo impl

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -24,50 +24,6 @@ pub trait CurveAffineExt: pasta_curves::arithmetic::CurveAffine {
     }
 }
 
-pub(crate) fn sqrt_tonelli_shanks<F: ff::PrimeField, S: AsRef<[u64]>>(
-    f: &F,
-    tm1d2: S,
-) -> CtOption<F> {
-    use subtle::ConstantTimeEq;
-
-    // w = self^((t - 1) // 2)
-    let w = f.pow_vartime(tm1d2);
-
-    let mut v = F::S;
-    let mut x = w * f;
-    let mut b = x * w;
-
-    // Initialize z as the 2^S root of unity.
-    let mut z = F::root_of_unity();
-
-    for max_v in (1..=F::S).rev() {
-        let mut k = 1;
-        let mut tmp = b.square();
-        let mut j_less_than_v: Choice = 1.into();
-
-        for j in 2..max_v {
-            let tmp_is_one = tmp.ct_eq(&F::one());
-            let squared = F::conditional_select(&tmp, &z, tmp_is_one).square();
-            tmp = F::conditional_select(&squared, &tmp, tmp_is_one);
-            let new_z = F::conditional_select(&z, &squared, tmp_is_one);
-            j_less_than_v &= !j.ct_eq(&v);
-            k = u32::conditional_select(&j, &k, tmp_is_one);
-            z = F::conditional_select(&z, &new_z, j_less_than_v);
-        }
-
-        let result = x * z;
-        x = F::conditional_select(&result, &x, b.ct_eq(&F::one()));
-        z = z.square();
-        b *= z;
-        v = k;
-    }
-
-    CtOption::new(
-        x,
-        (x * x).ct_eq(f), // Only return Some if it's the square root.
-    )
-}
-
 /// Compute a + b + carry, returning the result and the new carry over.
 #[inline(always)]
 pub(crate) const fn adc(a: u64, b: u64, carry: u64) -> (u64, u64) {
