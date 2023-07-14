5c24fbdfab6ef3ffc342dcc853b18fca9daf5536
kilic <kiliconu@itu.edu.tr>
2023-07-14 10:34:50 +0300

    Removes `CurveAffineExt` (#67)
    remove `CurveAffineExt`

--- src/pasta/mod.rs
+++ src/pasta/mod.rs
@@ -9,32 +9,6 @@ use ff::WithSmallOrderMulGroup;
 pub use pasta_curves::{pallas, vesta, Ep, EpAffine, Eq, EqAffine, Fp, Fq};
 use std::convert::TryInto;
 
-impl crate::CurveAffineExt for EpAffine {
-    fn batch_add<const COMPLETE: bool, const LOAD_POINTS: bool>(
-        _: &mut [Self],
-        _: &[u32],
-        _: usize,
-        _: usize,
-        _: &[Self],
-        _: &[u32],
-    ) {
-        unimplemented!();
-    }
-}
-
-impl crate::CurveAffineExt for EqAffine {
-    fn batch_add<const COMPLETE: bool, const LOAD_POINTS: bool>(
-        _: &mut [Self],
-        _: &[u32],
-        _: usize,
-        _: usize,
-        _: &[Self],
-        _: &[u32],
-    ) {
-        unimplemented!();
-    }
-}
-
 // Generated using https://github.com/ConsenSys/gnark-crypto/blob/master/ecc/utils.go
 // with `pasta_curves::Fp::ZETA`
 // See https://github.com/demining/Endomorphism-Secp256k1/blob/main/README.md
