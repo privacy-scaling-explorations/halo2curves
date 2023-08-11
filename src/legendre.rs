0694d0313c488293db0bc68373eef5769198fa97
David Nevado <david@davidnevado.xyz>
2023-08-11 22:00:41 +0200

    Add `Legendre` trait and macro
     - Add Legendre macro with norm and legendre symbol computation
     - Add macro for automatic implementation in prime fields

---
+++ src/legendre.rs
@@ -0,0 +1,35 @@
+use ff::{Field, PrimeField};
+
+pub trait Legendre: Field {
+    type BasePrimeField: PrimeField;
+
+    // This is (p-1)/2 where p is the modulus of the base prime field
+    fn legendre_exp() -> &'static Vec<u64>;
+
+    fn norm(&self) -> &Self::BasePrimeField;
+
+    fn legendre(&self) -> Self::BasePrimeField {
+        self.norm().pow(Self::legendre_exp())
+    }
+}
+
+#[macro_export]
+macro_rules! prime_field_legendre {
+    ($field:ident ) => {
+
+    lazy_static::lazy_static! {
+        static ref LE_AS_BIGUINT: BigUint = BigUint::from_bytes_le((-<$field as ff::Field>::ONE).to_repr().as_ref())/2usize ;
+        static ref LEGENDRE_EXP: Vec<u64> = LE_AS_BIGUINT.to_u64_digits();
+    }
+        impl crate::legendre::Legendre for $field {
+            type BasePrimeField = Self;
+
+            fn legendre_exp() -> &'static Vec<u64> {
+                &*LEGENDRE_EXP
+            }
+            fn norm(&self) -> &Self::BasePrimeField {
+                &self
+            }
+        }
+    };
+}
