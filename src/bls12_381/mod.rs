405f2c6c6ba4d55c3d757acce4715b41abf441d6
CPerezz <c.perezbaro@gmail.com>
2023-09-14 22:38:00 +0200

    refactor endo impl with failing tests

--- src/bls12_381/mod.rs
+++ src/bls12_381/mod.rs
@@ -9,20 +9,19 @@ use ff::PrimeField;
 use ff::WithSmallOrderMulGroup;
 use std::convert::TryInto;
 
-// Obtained from https://github.com/kilic/bls12-381/blob/master/glv.go and originally
-// generated using https://github.com/ConsenSys/gnark-crypto/blob/master/ecc/utils.go
+// Obtained from https://github.com/ConsenSys/gnark-crypto/blob/master/ecc/utils.go
 // See https://github.com/demining/Endomorphism-Secp256k1/blob/main/README.md
 // to have more details about the endomorphism.
 const ENDO_PARAMS_EP: EndoParameters = EndoParameters {
     // round(b2/n)
-    gamma1: [0x63f6e522f6cfee30, 0x7c6becf1e01faadd, 0x01],
+    gamma1: [0x63f6e522f6cfee30u64, 0x7c6becf1e01faadd, 0x01, 0x0],
     // round(-b1/n)
-    gamma2: [0x02, 0x0, 0x0, 0x0],
-    b1: [0x01, 0x0, 0x0, 0x0],
-    b2: [0x0000000100000000, 0xac45a4010001a402, 0x0, 0x0],
+    gamma2: [0x02u64, 0x0, 0x0, 0x0],
+    b1: [0x01u64, 0x0, 0x0, 0x0],
+    b2: [0x0000000100000000u64, 0xac45a4010001a402, 0x0, 0x0],
 };
 
-endo!(G1Projective, Fp, ENDO_PARAMS_EP);
+endo!(G1Projective, Scalar, ENDO_PARAMS_EP);
 
 #[test]
 fn test_endo() {
@@ -30,36 +29,54 @@ fn test_endo() {
     use rand_core::OsRng;
 
     for _ in 0..100000 {
-        let k = Fp::random(OsRng);
-        let (k1, k1_neg, k2, k2_neg) = Eq::decompose_scalar(&k);
+        let k = Scalar::random(OsRng);
+        let (k1, k1_neg, k2, k2_neg) = G1Projective::decompose_scalar(&k);
         if k1_neg & k2_neg {
-            assert_eq!(k, -Fp::from_u128(k1) + Fp::ZETA * Fp::from_u128(k2))
+            assert_eq!(
+                k,
+                -Scalar::from_u128(k1) + Scalar::ZETA * Scalar::from_u128(k2)
+            )
         } else if k1_neg {
-            assert_eq!(k, -Fp::from_u128(k1) - Fp::ZETA * Fp::from_u128(k2))
+            assert_eq!(
+                k,
+                -Scalar::from_u128(k1) - Scalar::ZETA * Scalar::from_u128(k2)
+            )
         } else if k2_neg {
-            assert_eq!(k, Fp::from_u128(k1) + Fp::ZETA * Fp::from_u128(k2))
+            assert_eq!(
+                k,
+                Scalar::from_u128(k1) + Scalar::ZETA * Scalar::from_u128(k2)
+            )
         } else {
-            assert_eq!(k, Fp::from_u128(k1) - Fp::ZETA * Fp::from_u128(k2))
+            assert_eq!(
+                k,
+                Scalar::from_u128(k1) - Scalar::ZETA * Scalar::from_u128(k2)
+            )
         }
     }
 
     for _ in 0..100000 {
-        let k = Fp::random(OsRng);
-        let (k1, k1_neg, k2, k2_neg) = Eq::decompose_scalar(&k);
+        let k = Scalar::random(OsRng);
+        let (k1, k1_neg, k2, k2_neg) = G1Projective::decompose_scalar(&k);
         if k1_neg & k2_neg {
-            assert_eq!(k, -Fp::from_u128(k1) + Fp::ZETA * Fp::from_u128(k2))
+            assert_eq!(
+                k,
+                -Scalar::from_u128(k1) + Scalar::ZETA * Scalar::from_u128(k2)
+            )
         } else if k1_neg {
-            assert_eq!(k, -Fp::from_u128(k1) - Fp::ZETA * Fp::from_u128(k2))
+            assert_eq!(
+                k,
+                -Scalar::from_u128(k1) - Scalar::ZETA * Scalar::from_u128(k2)
+            )
         } else if k2_neg {
-            assert_eq!(k, Fp::from_u128(k1) + Fp::ZETA * Fp::from_u128(k2))
+            assert_eq!(
+                k,
+                Scalar::from_u128(k1) + Scalar::ZETA * Scalar::from_u128(k2)
+            )
         } else {
-            assert_eq!(k, Fp::from_u128(k1) - Fp::ZETA * Fp::from_u128(k2))
+            assert_eq!(
+                k,
+                Scalar::from_u128(k1) - Scalar::ZETA * Scalar::from_u128(k2)
+            )
         }
     }
-}
-
-#[test]
-fn test_quadratic_residue() {
-    crate::tests::field::random_quadratic_residue_test::<Fp>();
-    crate::tests::field::random_quadratic_residue_test::<Fq>();
 }
