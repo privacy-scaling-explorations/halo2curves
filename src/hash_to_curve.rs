3ce2bffc328b86076481f33347603b8669a72802
duguorong <dgr93930@gmail.com>
2023-12-05 22:45:11 +0800

    feat: add "iso_map" for secp256k1 h2c func

--- src/hash_to_curve.rs
+++ src/hash_to_curve.rs
@@ -173,6 +173,106 @@ where
     })
 }
 
+/// 3-Isogeny Map for Secp256k1
+/// Reference: <https://www.rfc-editor.org/rfc/rfc9380.html#name-3-isogeny-map-for-secp256k1>
+fn iso_map_secp256k1<C>(x: C::Base, y: C::Base) -> (C::Base, C::Base)
+where
+    C: CurveExt,
+    C::Base: FromUniformBytes<64>,
+{
+    // constants for secp256k1 iso_map computation
+    const K: [[&str; 4]; 5] = [
+        ["0x00", "0x00", "0x00", "0x00"],
+        [
+            "0x8e38e38e38e38e38e38e38e38e38e38e38e38e38e38e38e38e38e38daaaaa8c7",
+            "0x7d3d4c80bc321d5b9f315cea7fd44c5d595d2fc0bf63b92dfff1044f17c6581",
+            "0x534c328d23f234e6e2a413deca25caece4506144037c40314ecbd0b53d9dd262",
+            "0x8e38e38e38e38e38e38e38e38e38e38e38e38e38e38e38e38e38e38daaaaa88c",
+        ],
+        [
+            "0xd35771193d94918a9ca34ccbb7b640dd86cd409542f8487d9fe6b745781eb49b",
+            "0xedadc6f64383dc1df7c4b2d51b54225406d36b641f5e41bbc52a56612a8c6d14",
+            "0x00",
+            "0x00",
+        ],
+        [
+            "0x4bda12f684bda12f684bda12f684bda12f684bda12f684bda12f684b8e38e23c",
+            "0xc75e0c32d5cb7c0fa9d0a54b12a0a6d5647ab046d686da6fdffc90fc201d71a3",
+            "0x29a6194691f91a73715209ef6512e576722830a201be2018a765e85a9ecee931",
+            "0x2f684bda12f684bda12f684bda12f684bda12f684bda12f684bda12f38e38d84",
+        ],
+        [
+            "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffff93b",
+            "0x7a06534bb8bdb49fd5e9e6632722c2989467c1bfc8e8d978dfb425d2685c2573",
+            "0x6484aa716545ca2cf3a70c3fa8fe337e0a3d21162f0d6299a7bf8192bfd2a76f",
+            "0x00",
+        ],
+    ];
+    let mut k: [[C::Base; 4]; 5] = [[C::Base::from_uniform_bytes(&[0; 64]); 4]; 5];
+    for i in 0..5 {
+        for j in 0..4 {
+            k[i][j] = C::Base::from_uniform_bytes(&hex_str_to_le_bytes(K[i][j]));
+        }
+    }
+
+    let x_squared = x.square();
+    let x_cubed = x * x_squared;
+
+    let x_num = k[1][3] * x_cubed + k[1][2] * x_squared + k[1][1] * x + k[1][0];
+    let x_den = x_squared + k[2][1] * x + k[2][0];
+
+    let y_num = k[3][3] * x_cubed + k[3][2] * x_squared + k[3][1] * x + k[3][0];
+    let y_den = x_cubed + k[4][2] * x_squared + k[4][1] * x + k[4][0];
+
+    // Exceptional case MUST return identity
+    // reference: <https://www.rfc-editor.org/rfc/rfc9380.html#name-simplified-swu-for-ab-0>
+    if x_den.is_zero().into() || y_den.is_zero().into() {
+        return (C::Base::ZERO, C::Base::ZERO);
+    }
+
+    let x = x_num * x_den.invert().unwrap();
+    let y = y * (y_num * y_den.invert().unwrap());
+
+    (x, y)
+}
+
+/// Convert hex string to little-endian bytes array of length `L`
+///
+/// NOTE: hex string should be prefixed with "0x"
+///
+/// Example:  
+///
+///   hex_str_to_le_bytes::<4>("0x01020304") -> [4, 3, 2, 1]
+///
+///   hex_str_to_le_bytes::<6>("0x01020304") -> [4, 3, 2, 1, 0, 0]
+///
+///   hex_str_to_le_bytes::<2>("0x01020304") -> [4, 3]
+///
+fn hex_str_to_le_bytes<const L: usize>(hex: &str) -> [u8; L] {
+    let padded_hex_string = if hex.len() % 2 != 0 {
+        format!("0{}", &hex[2..])
+    } else {
+        hex[2..].to_owned()
+    };
+
+    // Convert each pair of hex characters to u8 and collect into a vector
+    let le_bytes: Result<Vec<u8>, _> = (0..padded_hex_string.len())
+        .step_by(2)
+        .rev() // Iterate in reverse order for little-endian byte order
+        .map(|i| {
+            u8::from_str_radix(&padded_hex_string[i..i + 2], 16)
+                .map_err(|_| "Invalid hex character")
+        })
+        .collect();
+    let le_bytes = le_bytes.expect("Invalid bytes");
+
+    let mut result = [0; L];
+    for i in 0..L.min(le_bytes.len()) {
+        result[i] = le_bytes[i];
+    }
+    result
+}
+
 #[allow(clippy::too_many_arguments)]
 pub(crate) fn svdw_map_to_curve<C>(
     u: C::Base,
