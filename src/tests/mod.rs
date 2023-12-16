4ed6bb1e599fb0e1928fad78a8286551744aaf45
duguorong <dgr93930@gmail.com>
2023-12-16 12:52:06 +0800

    refactor: create "utils" module & move "fe_from_str"

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,17 +1,2 @@
-use ff::PrimeField;
-use num_bigint::BigUint;
-use num_traits::Num;
-use std::borrow::Cow;
-
 pub mod curve;
 pub mod field;
-
-pub(crate) fn fe_from_str<F: PrimeField>(string: impl AsRef<str>) -> F {
-    let string = string.as_ref();
-    let oct = if let Some(hex) = string.strip_prefix("0x") {
-        Cow::Owned(BigUint::from_str_radix(hex, 16).unwrap().to_string())
-    } else {
-        Cow::Borrowed(string)
-    };
-    F::from_str_vartime(&oct).unwrap()
-}
