906937cbe073538a2297a795556e39220093f618
kilic <onurkilic1004@gmail.com>
2024-01-10 14:16:38 +0300

    remove multicore feature in msm bench
    remove useless prefetch feature

--- src/lib.rs
+++ src/lib.rs
@@ -24,15 +24,3 @@ pub use pasta_curves::arithmetic::{Coordinates, CurveAffine, CurveExt};
 
 #[cfg(test)]
 pub mod tests;
-
-#[cfg(all(feature = "prefetch", target_arch = "x86_64"))]
-#[inline(always)]
-pub fn prefetch<T>(data: &[T], offset: usize) {
-    use core::arch::x86_64::_mm_prefetch;
-    unsafe {
-        _mm_prefetch(
-            data.as_ptr().add(offset) as *const i8,
-            core::arch::x86_64::_MM_HINT_T0,
-        );
-    }
-}
