089f456753d4df1b65f882e4fe38aeca95178522
Srinath Setty <srinath@microsoft.com>
2025-06-12 10:17:50 -0700

    move comment up

--- src/t256/mod.rs
+++ src/t256/mod.rs
@@ -1,6 +1,7 @@
 mod curve;
 mod fp;
 
+// the base field of secp256r1 is Fq
 pub use crate::secp256r1::Fp as Fq;
 pub use curve::*;
-pub use fp::*; // the base field of secp256r1 is Fq
+pub use fp::*;
